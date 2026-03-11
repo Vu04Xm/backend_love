d921aabe11fa73379d9b459a83d1c1d91a407866
VuDao204 <vu2004kui2@gmail.com>
2026-03-12 00:49:08 +0700

    Fix: Cấu hình lại biến môi trường và kết nối DB

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+    const mysql = require('mysql2');
+    require('dotenv').config();
 
-const pool = mysql.createPool({
-  // Thêm giá trị mặc định vào sau dấu || để test nhanh
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USER || 'avnadmin',
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'defaultdb',
-  port: process.env.DB_PORT || 25363,
-  ssl: { rejectUnauthorized: false }
-});
+    // Tạo pool kết nối để tối ưu hiệu suất
+    const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 25367, // Thêm cổng của bạn vào đây
+    ssl: {
+        rejectUnauthorized: false // Bắt buộc phải có để kết nối tới Aiven/Cloud DB
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+    });
 
-module.exports = pool.promise();
+    module.exports = pool.promise();
